73dec918dc26fc8b2b41187f99b4d8ebba4a533b
Stas B <s.belichenko@studio-sold.ru>
2019-01-29 10:14:33 +0300

    Started using CommonHelpers on admin page

--- admin/priceva_bc.php
+++ admin/priceva_bc.php
@@ -14,15 +14,15 @@ require_once( $_SERVER[ "DOCUMENT_ROOT" ] . "/bitrix/modules/" . $MODULE_ID . "/
 CModule::IncludeModule($MODULE_ID);
 
 
-$helpers = \Priceva\Connector\Bitrix\Helpers::getInstance();
+$common_helpers = \Priceva\Connector\Bitrix\Helpers\CommonHelpers::getInstance();
 
-$FORM_RIGHT = $helpers->APPLICATION->GetGroupRight($MODULE_ID);
-if( $FORM_RIGHT <= "D" ) $helpers->APPLICATION->AuthForm(GetMessage("ACCESS_DENIED"));
+$FORM_RIGHT = $common_helpers->APPLICATION->GetGroupRight($MODULE_ID);
+if( $FORM_RIGHT <= "D" ) $common_helpers->APPLICATION->AuthForm(GetMessage("ACCESS_DENIED"));
 
 require( $_SERVER[ "DOCUMENT_ROOT" ] . "/bitrix/modules/main/include/prolog_admin_after.php" );
 
-if( "POST" === $helpers->request_method() ){
-    if( $helpers->app->getContext()->getRequest()->getPost('run') ){
+if( "POST" === $common_helpers->request_method() ){
+    if( $common_helpers->app->getContext()->getRequest()->getPost('run') ){
         $connector = new \Priceva\Connector\Bitrix\PricevaConnector();
         $connector->run();
         $errors = $connector->get_last_info_msg();
@@ -31,7 +31,8 @@ if( "POST" === $helpers->request_method() ){
 
 ?>
 <?=$errors?>
-<form method="post" action="<? echo $helpers->APPLICATION->GetCurPage() ?>?lang=<?=LANGUAGE_ID?>" id="priceva_bc">
+    <form method="post" action="<? echo $common_helpers->APPLICATION->GetCurPage() ?>?lang=<?=LANGUAGE_ID?>"
+          id="priceva_bc">
     <input type="submit" name="run" value="Запустить синхронизацию" title="Запустить синхронизацию ">
 </form>
 
