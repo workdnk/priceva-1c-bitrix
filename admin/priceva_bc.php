e20cc4f1ce46fb5f1a610941ddc93a4e0cefba71
Stas B <s.belichenko@studio-sold.ru>
2019-01-29 16:01:04 +0300

    A little more beauty on the admin page

--- admin/priceva_bc.php
+++ admin/priceva_bc.php
@@ -25,12 +25,13 @@ if( "POST" === $common_helpers->request_method() ){
     if( $common_helpers->app->getContext()->getRequest()->getPost('run') ){
         $connector = new \Priceva\Connector\Bitrix\PricevaConnector();
         $connector->run();
-        $errors = $connector->get_last_info_msg();
+        $info = $connector->get_last_info_msg();
     }
 }
 
 ?>
-<?=$errors?>
+<?="<div id='info'>" . $info . "<br /><br /></div>"?>
+
     <form method="post" action="<? echo $common_helpers->APPLICATION->GetCurPage() ?>?lang=<?=LANGUAGE_ID?>"
           id="priceva_bc">
     <input type="submit" name="run" value="Запустить синхронизацию" title="Запустить синхронизацию ">
