7f13ac44d4ffc76b5609b68f00d28f8e9b5196af
Stas B <s.belichenko@studio-sold.ru>
2019-01-30 19:36:58 +0300

    + to 990c896b83b585dfa1b67c96242e741d630d4afd

--- lang/ru/lib/bitrix/pricevaconnector.php
+++ lang/ru/lib/bitrix/pricevaconnector.php
@@ -4,3 +4,5 @@ $MESS[ "PRICEVA_BC_INFO_TEXT2" ] = "не найдено товаров в Bitrix
 $MESS[ "PRICEVA_BC_INFO_TEXT3" ] = "нулевых цен в Priceva";
 $MESS[ "PRICEVA_BC_INFO_TEXT4" ] = "успешно обновленных товаров";
 $MESS[ "PRICEVA_BC_INFO_TEXT5" ] = "ошибок при обновлении товаров";
+$MESS[ "PRICEVA_BC_INFO_TEXT6" ] = "пустой артикул в товаре из Priceva";
+$MESS[ "PRICEVA_BC_INFO_TEXT7" ] = "пустой артикул в товаре из Bitrix";
