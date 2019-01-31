d077df9ea12a559383f5320a955659d35861aaea
Stas B <s.belichenko@studio-sold.ru>
2019-01-31 11:26:20 +0300

    Swapped encodings in utils/converter.sh

--- utils/converter.sh
+++ utils/converter.sh
@@ -10,6 +10,6 @@ while read file
 do
   echo " $file"
   mv $file $file.icv
-  iconv -f WINDOWS-1251 -t UTF-8 $file.icv > $file
+  iconv -f UTF-8 -t WINDOWS-1251 $file.icv > $file
   rm -f $file.icv
 done
