bbad42fc6afe2000b41dc10c2101e3cc8d6be150
Stas B <s.belichenko@studio-sold.ru>
2019-02-11 14:00:07 +0300

    Обновили версию модуля

--- install/version.php
+++ install/version.php
@@ -7,6 +7,6 @@
  */
 
 $arModuleVersion = array(
-    "VERSION"      => "0.2.6",
-    "VERSION_DATE" => "2019-02-08 15:00:00",
+    "VERSION"      => "0.2.7",
+    "VERSION_DATE" => "2019-02-11 13:00:00",
 );
