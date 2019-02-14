0259c9080b3f57ec99479c23824ca961d698dc07
Stas B <s.belichenko@studio-sold.ru>
2019-02-14 15:51:28 +0300

    More accurate display of module deletion notifications

--- install/unstep1.php
+++ install/unstep1.php
@@ -12,24 +12,24 @@ use Bitrix\Main\ModuleManager;
 if( !check_bitrix_sessid() ) return;
 
 Loc::loadMessages(__FILE__);
+?>
 
-echo ( new CAdminMessage([
-    "MESSAGE" => Loc::getMessage("PRICEVA_BC_UNINSTALL_DELETE"),
-    "TYPE"    => "OK",
-]) )->Show(); ?>
-
-<form action="<? echo $APPLICATION->GetCurPage(); ?>">
-    <input type="hidden" name="lang" value="<? echo LANG ?>">
-    <?php
-    /**
-     * @noinspection PhpUndefinedClassInspection
-     * @var priceva_connector $Module
-     */
-    echo ( new CAdminMessage($Module->get_info('deleted_price')) )->Show() ?>
-    <div>
-        <input type="submit" name="" value="<? echo Loc::getMessage("MOD_BACK"); ?>">
-    </div>
-</form>
+    <form action="<? echo $APPLICATION->GetCurPage(); ?>">
+        <input type="hidden" name="lang" value="<? echo LANG ?>">
+        <?php
+        echo ( new CAdminMessage([
+            "MESSAGE" => Loc::getMessage("PRICEVA_BC_UNINSTALL_DELETE"),
+            "TYPE"    => "OK",
+        ]) )->Show();
+        /**
+         * @noinspection PhpUndefinedClassInspection
+         * @var priceva_connector $Module
+         */
+        echo ( new CAdminMessage($Module->get_info('deleted_price')) )->Show() ?>
+        <div>
+            <input type="submit" name="" value="<? echo Loc::getMessage("MOD_BACK"); ?>">
+        </div>
+    </form>
 
 <?php
 ModuleManager::unRegisterModule($Module->get_info('module_id'));
