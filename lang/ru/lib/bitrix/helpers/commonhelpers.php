e4942e17e7ae47abbd903b856ebba38fbc4e70e7
Stas B <s.belichenko@studio-sold.ru>
2019-02-14 17:51:12 +0300

    Insert capability "choose later" option into selects

--- lang/ru/lib/bitrix/helpers/commonhelpers.php
+++ lang/ru/lib/bitrix/helpers/commonhelpers.php
@@ -1,3 +1,4 @@
 <?
 $MESS[ "PRICEVA_BC_COMMON_HELPERS_NOT_SELECTED" ]                = "Не выбрано";
+$MESS[ "PRICEVA_BC_COMMON_HELPERS_NOT_SELECT_LATER" ]            = "Выбрать позднее";
 $MESS[ "PRICEVA_BC_INSTALL_ERROR_MODULE_CATALOG_NOT_INSTALLED" ] = "Модуль catalog не установлен. Работа модуля PRICEVA невозможна";
