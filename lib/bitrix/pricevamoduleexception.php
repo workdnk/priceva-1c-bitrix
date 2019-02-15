90d52537e95d18ad43715c15a69194e34afda770
Stas B <s.belichenko@studio-sold.ru>
2019-02-15 20:21:04 +0300

    New exception class for only module errors

---
+++ lib/bitrix/pricevamoduleexception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: S.Belichenko, email: stanislav@priceva.com
+ * Date: 15.02.2019
+ * Time: 12:34
+ */
+
+namespace Priceva\Connector\Bitrix;
+
+class PricevaModuleException extends \Exception
+{
+
+}
