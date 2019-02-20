2c6cf0d42e8afa1bc8af77893c270419a0775546
Stas B <s.belichenko@studio-sold.ru>
2019-02-20 12:56:43 +0300

    Correct version

--- install/version.php
+++ install/version.php
@@ -7,6 +7,6 @@
  */
 
 $arModuleVersion = array(
-    "VERSION"      => "1.2.0",
-    "VERSION_DATE" => "2019-02-20 12:00:00",
+    "VERSION"      => "1.3.0",
+    "VERSION_DATE" => "2019-02-20 13:00:00",
 );
