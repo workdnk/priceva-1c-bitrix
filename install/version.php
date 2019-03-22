2b725edf9f37b1a71ff32e41abeb4ece067b8de3
Stas B <s.belichenko@studio-sold.ru>
2019-03-22 16:54:09 +0300

    New version: 1.7.0

--- install/version.php
+++ install/version.php
@@ -7,6 +7,6 @@
  */
 
 $arModuleVersion = array(
-    "VERSION"      => "1.6.1",
-    "VERSION_DATE" => "2019-03-22 13:00:00",
+    "VERSION"      => "1.7.0",
+    "VERSION_DATE" => "2019-03-22 16:00:00",
 );
